139eeb613983d12d82c5dd80e459740a70abedb0
touchmore <touchmore@sohu.com>
2016-01-09 16:59:24 +0800

    正写幻灯片

--- src/com/kb/teademo/MainActivity.java
+++ src/com/kb/teademo/MainActivity.java
@@ -1,19 +1,36 @@
 package com.kb.teademo;
 
-import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.Menu;
+import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.kb.teademo.fragment.Fragment_tops;
+
+public class MainActivity extends FragmentActivity {
 	private HorizontalScrollView horizontalScrollView;
 	private LinearLayout linearLayout;
+	private ViewPager viewPager;
+	private Fragment fragment;
+	private FragmentPagerAdapter fragmentPagerAdapter;
 
 	private void initView() {
 		horizontalScrollView = (HorizontalScrollView) findViewById(R.id.hs_main);// 导航栏
 		linearLayout = (LinearLayout) findViewById(R.id.ll_main);
-
+		viewPager = (ViewPager) findViewById(R.id.vp_main);
+		for (int i = 0; i < linearLayout.getChildCount(); i++) {
+			TextView textView = (TextView) linearLayout.getChildAt(i);
+			textView.setTextColor(0xff000000);
+		}
+		((TextView) linearLayout.getChildAt(0)).setTextColor(Color.GREEN);
 	}
 
 	@Override
@@ -21,7 +38,69 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		initView();
+		viewPager.setOnPageChangeListener(new OnPageChangeListener() {
 
+			@Override
+			public void onPageSelected(int arg0) {
+				// TODO Auto-generated method stub
+				// 触发导航栏，变化颜色及位置
+				horizontalScrollView.smoothScrollTo(100 * arg0, 0);
+				for (int i = 0; i < linearLayout.getChildCount(); i++) {
+					TextView textView = (TextView) linearLayout.getChildAt(i);
+					textView.setTextColor(0xff000000);
+				}
+				((TextView) linearLayout.getChildAt(arg0))
+						.setTextColor(Color.GREEN);
+
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		fragmentPagerAdapter = new FragmentPagerAdapter(
+				getSupportFragmentManager()) {
+
+			@Override
+			public int getCount() {
+				// TODO Auto-generated method stub
+				return linearLayout.getChildCount();
+			}
+
+			@Override
+			public Fragment getItem(int arg0) {
+				// TODO Auto-generated method stub
+				switch (arg0) {
+				case 0:
+					fragment = new Fragment_tops();
+					break;
+				case 1:
+					fragment = new Fragment_tops();
+				case 2:
+					fragment = new Fragment_tops();
+				case 3:
+					fragment = new Fragment_tops();
+				case 4:
+					fragment = new Fragment_tops();
+
+				default:
+					break;
+				}
+
+				return fragment;
+			}
+		};
+
+		viewPager.setAdapter(fragmentPagerAdapter);
 	}
 
 	@Override
@@ -31,4 +110,15 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void onClick(View view) {
+		int n = linearLayout.indexOfChild(view);
+		// 变化颜色
+		for (int i = 0; i < linearLayout.getChildCount(); i++) {
+			TextView textView = (TextView) linearLayout.getChildAt(i);
+			textView.setTextColor(0xff000000);
+		}
+		((TextView) linearLayout.getChildAt(n)).setTextColor(Color.GREEN);
+		// 变化页面
+		viewPager.setCurrentItem(linearLayout.indexOfChild(view));
+	}
 }
--- src/com/kb/teademo/fragment/Fragment_tops.java
+++ src/com/kb/teademo/fragment/Fragment_tops.java
@@ -2,18 +2,73 @@ package com.kb.teademo.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.kb.teademo.R;
+import com.kb.teademo.asynctask.LoadPicAsyncTask;
+import com.kb.teademo.config.Config;
 
 public class Fragment_tops extends Fragment {
+	private ViewPager viewPager;// 广告轮播
+	private LinearLayout linearLayout;// 广告指示小点
+	private TextView textView;// 广告信息
+	private ListView listView;// 列表
+	private View view;
+	private PagerAdapter pagerAdapter;
+
+	private void initView() {
+		viewPager = (ViewPager) view.findViewById(R.id.vp_tops);
+		linearLayout = (LinearLayout) view.findViewById(R.id.ll_ad_tops);
+		textView = (TextView) view.findViewById(R.id.tv_ad_tops);
+		listView = (ListView) view.findViewById(R.id.lv_tops);
+
+	}
+
+	private void initCtrl() {
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View view = View.inflate(getActivity(), R.layout.fragment_tops, null);
-		return super.onCreateView(inflater, container, savedInstanceState);
+		view = View.inflate(getActivity(), R.layout.fragment_tops, null);
+		initView();
+		// 为幻灯片ViewPager添加监听
+		viewPager.setOnPageChangeListener(new OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int arg0) {
+				// TODO Auto-generated method stub
+				// 触发小点变换
+				for (int i = 0; i < linearLayout.getChildCount(); i++) {
+					linearLayout.getChildAt(i).setSelected(false);
+				}
+				linearLayout.getChildAt(arg0).setSelected(true);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		// 加载异步任务：网络获取，并加载图片
+		new LoadPicAsyncTask(viewPager, getActivity()).execute(Config.HOMEPATH);
+
+		return view;
 	}
 }
